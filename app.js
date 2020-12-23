4f50171edd5ff42d740774b8695abb1947997c38
Fatensamman <arch.fatensamman@gmail.com>
2020-12-23 16:23:23 +0200

    add msg

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 alert('Hello! welcome to know more about food ^___^');
 
-var YourAge = prompt('Please enter your age');
+var yourage = prompt('Please enter your age');
 var message ;
 
-if (YourAge<45) {message = ' Choose a variety of vegetables, including dark green, red and orange, beans and peas, starchy and non-starchy.Eat a variety of fruits.Include grains in your daily diet . Half of your grains should be whole grains.Stick to fat -free or low-fat dairy. Have protein at every meal. Use healthy oils, such as olive oil.'
+if (yourage>45) {
+  message = ' Choose a variety of vegetables, including dark green, red and orange, beans and peas, starchy and non-starchy.Eat a variety of fruits.Include grains in your daily diet . Half of your grains should be whole grains.Stick to fat -free or low-fat dairy. Have protein at every meal. Use healthy oils, such as olive oil.'
     
-} else {'CANNED SOUP. Canned Soup. POP-TARTS& BREAKFAST PASTRIES.SPECIAL K PROTEIN BARS. Special K bar.COLA. Soda.OREOS. Oreos. COCKTAILS & BEER.10&11. BAGELS & WHITE BREAD'
+} else {
+  message = 'CANNED SOUP. Canned Soup. POP-TARTS& BREAKFAST PASTRIES.SPECIAL K PROTEIN BARS. Special K bar.COLA. Soda.OREOS. Oreos. COCKTAILS & BEER.10&11. BAGELS & WHITE BREAD'
     
 };
   alert (message);
-  alert ('Thank you');
+  alert ('Thank you ^__^');
 
   
