88039803819fe7c1fc184267924582ad4e18c87f
Fatensamman <arch.fatensamman@gmail.com>
2020-12-24 17:43:04 +0200

    add loop

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 
+ 
+ alert('Hello! welcome to know more about food ^___^');
+
+ var user = prompt ('please enter your name')
+ while (user === '' || user === null){
+     user = prompt ('please enter your name')
+ }
+
+   document.write(user + '<h4>'+ 'If more of us valued food and cheer and song above hoarded gold, it would be a merrier world '+'</h>')
+ 
 var yourage = prompt('Please enter your age to know what kind of food you can eat');
 var message ;
 
+while (yourage === ""|| yourage === null){
+  yourage = prompt('Please enter your age to know what kind of food you can eat');
+}
 if (yourage>45) {
   message = ' Kinds of food you can eat :Choose a variety of vegetables, including dark green, red and orange, beans and peas, starchy and non-starchy.Eat a variety of fruits.Include grains in your daily diet . Half of your grains should be whole grains.Stick to fat -free or low-fat dairy. Have protein at every meal. Use healthy oils, such as olive oil.'
     
@@ -12,18 +25,33 @@ if (yourage>45) {
   alert (message);
 
   var foodhub;
-
-  var yn = prompt('do you wantt to get notification?? ok or cancel ');
  
-if (confirm (yn) == true) {
-    foodhub = "You will be notified!";
-} else {
-   foodhub = "as you like!"
+  
+  var askfood = prompt('Please enter the number of kinds of cheese do you prefer?')
+  for  ( var i=1 ; i<= askfood ; i++){
+    document.write('<p>'+ i  +'</p>' + '<img src="https://images-prod.healthline.com/hlcmsresource/images/AN_images/healthiest-cheese-1296x728-swiss.jpg"style=width:600px;height:600px/>')
+ 
   }
 
-
-
+  var yn = prompt('do you want to get notification?? yes or no please ');
+  while (yn === '' || yn === null) {
+    yn = prompt('do you want to get notification?? yes or no please ');
+  }
+  
+  while (yn !== 'yes' && yn !=='no' ){
+    yn = prompt('do you want to get notification?? yes or no please ');
+  }
+  
+if (yn == 'yes') {
+    foodhub = "You will be notified!";
+} else { if( yn == 'no'){
+  foodhub = "as you like!"
+}
+  }
   alert (foodhub);
+
+
+ 
   
   alert ('Thank you ^__^');
 
